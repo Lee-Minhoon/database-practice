14ae0c451bcc8ec17057647a5bbbcda7b4511432
Lee Minhoon <hackids@naver.com>
2021-11-03 10:33:28 +0900

    init commit

--- FinalTest/include/menu.php
+++
@@ -1,5 +0,0 @@
-	<section>
-		<button type="button" onclick="location.href = './index.php'">홈</button>
-		<button type="button" onclick="location.href = './mypage.php'">마이페이지</button>
-		<button type="button" onclick="location.href = './logout.php'">로그아웃</button>
-	</section>
